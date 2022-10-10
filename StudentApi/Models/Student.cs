910cc14149ff152452e9fe168af8652ee9c2a677
Wilfred Opudo <opudowilfred.ow@gmail.com>
2022-10-10 21:48:27 +0300

    add initial project files

---
+++ StudentApi/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApi.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string EnrollmentDate { get; set; }
+        public string Course { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
